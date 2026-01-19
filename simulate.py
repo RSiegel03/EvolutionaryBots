db2156327d85643355e9b1818412459bc41377b1
RSiegel03 <ryancgl26@gmail.com>
2026-01-19 13:19:19 -0500

    onelink made

--- simulate.py
+++ simulate.py
@@ -7,7 +7,11 @@ if __name__ == "__main__":
     physicsClient = p.connect(p.GUI)    
     # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
-    for i in range(1000):
+    p.loadSDF("box.sdf")
+    p.setGravity(0,0,-10)
+
+
+    for i in range(500):
         p.stepSimulation()
         time.sleep(1/60)
         print("Simulation step:", i)
