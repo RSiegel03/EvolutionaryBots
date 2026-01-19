cc1381d23d407eb67d45c16e871e9e3ea66bcfaa
RSiegel03 <ryancgl26@gmail.com>
2026-01-19 13:53:00 -0500

    tower made

--- generate.py
+++ generate.py
@@ -4,14 +4,14 @@ if __name__ == "__main__":
     pyrosim.Start_SDF("boxes.sdf") # name of file to name world
 
     box_ct = 10
-    length, width, height = 1, 1, 1
-    x, y, z = 0, 0, 0.5
     for i in range(box_ct):
+        # update for next box
+        length, width, height = 0.9**(i), 0.9**(i), 0.9**(i)
+        x, y, z = 0, 0, 0.5 + 1*i
+
         pyrosim.Send_Cube(name="Box" + str(i), pos=[x,y,z], 
                           size=[length,width,height])
         
-        # update for next box
-        length, width, height = 0.9**(i+1), 0.9**(i+1), 0.9**(i+1)
-        z += 1
+        
 
     pyrosim.End()
