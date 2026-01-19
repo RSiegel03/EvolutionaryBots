85a16cbecef307341eed621bf9375d32e142bef5
RSiegel03 <ryancgl26@gmail.com>
2026-01-19 12:52:35 -0500

    A01 done

--- simulate.py
+++ simulate.py
@@ -4,8 +4,8 @@ import time
 
 
 if __name__ == "__main__":
-    physicsClient = p.connect(p.GUI)
-    p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+    physicsClient = p.connect(p.GUI)    
+    # p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 
     for i in range(1000):
         p.stepSimulation()
