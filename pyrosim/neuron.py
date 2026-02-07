05b84b37187bad691fdfb6752976a0e6ff3343c5
RSiegel03 <rdsiegel@uvm.edu>
2026-02-07 18:47:07 -0500

    closed loop control w synapses

--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -69,9 +69,17 @@ class NEURON:
     def Update_Sensor_Neuron(self):
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
 
-    def Update_Hidden_Or_Motor_Neuron(self):
-        # Placeholder for future neural network processing
+    def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
+        
         self.Set_Value(0.0)
+        for synapse in synapses:
+            if synapse[1] == self.Get_Name():
+                self.Allow_Presynaptic_Neuron_To_Influence_Me(synapses[synapse].Get_Weight(), neurons[synapse[0]].Get_Value())
+
+        self.Threshold()
+
+    def Allow_Presynaptic_Neuron_To_Influence_Me(self, synaptic_weight, presynaptic_neuron_value):
+        self.Add_To_Value( synaptic_weight * presynaptic_neuron_value )
 
 # -------------------------- Private methods -------------------------
 
--- simulate.py
+++ simulate.py
@@ -4,4 +4,3 @@ if __name__ == "__main__":
     simulation = SIMULATION()
 
     simulation.Run()
-
